3609ff0397e3fbfb5b757b2c19c6f9aedf0803d4
Alec M. <alecmattu1234@gmail.com>
2025-02-01 14:23:20 -0500

    Cleanup unused code

--- common/tcp.go
+++ common/tcp.go
@@ -13,89 +13,55 @@ import (
 
 // TCPStream connects to the liveview server using a TCP connection
 // TODO: Support multiple output methods (e.g. ffmpeg, ffplay, etc.)
+// TODO: Support audio I/O
+// TODO: Support command I/O (e.g. PTZ commands)
 //
 // connInfo: the connection details to use to connect to the liveview server
 //
 // Example: TCPStream(ConnectionDetails{Host: "example.com", Port: "443", ConnectionId: 1234, ClientId: 5678})
 func TCPStream(connInfo ConnectionDetails) {
-	fmt.Printf("Connecting to %s:%s\n", connInfo.Host, connInfo.Port)
+	fmt.Printf("Initializing stream to %s:%s\n", connInfo.Host, connInfo.Port)
 
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", connInfo.Host, connInfo.Port))
+	client, err := tls.Dial("tcp", fmt.Sprintf("%s:%s", connInfo.Host, connInfo.Port), &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         connInfo.Host,
+		Certificates:       []tls.Certificate{},
+	})
 	if err != nil {
 		fmt.Println("Error connecting:", err)
 		return
-	}
-	defer conn.Close()
-
-	client := tls.Client(conn, &tls.Config{
-		InsecureSkipVerify: true,
-		ServerName:         connInfo.Host,
-	})
-
-	err = conn.SetReadDeadline(time.Now().Add(3 * time.Second))
-	if err != nil {
-		fmt.Println("Error setting read deadline:", err)
-		return
-	}
-	err = conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
-	if err != nil {
-		fmt.Println("Error setting write deadline:", err)
-		return
-	}
-
-	err = client.Handshake()
-	if err != nil {
-		fmt.Println("TLS handshake failed:", err)
-		return
 	} else {
-		fmt.Println("TLS handshake successful")
+		fmt.Println("Connected to", client.RemoteAddr())
 	}
 	defer client.Close()
 
-	err = conn.SetReadDeadline(time.Now().Add(3 * time.Second))
-	if err != nil {
-		fmt.Println("Error setting read deadline:", err)
-		return
-	}
-	err = conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
-	if err != nil {
-		fmt.Println("Error setting write deadline:", err)
-		return
-	}
-
 	ffplayCmd := exec.Command("ffplay", "-f", "mpegts", "-err_detect", "ignore_err", "-")
-	ffplayIn, err := ffplayCmd.StdinPipe()
+	inputPipe, err := ffplayCmd.StdinPipe()
 	if err != nil {
 		fmt.Println("Error creating ffplay stdin pipe:", err)
 		return
-	} else {
-		fmt.Println("Created ffplay stdin pipe")
 	}
 
 	err = ffplayCmd.Start()
 	if err != nil {
 		fmt.Println("Error starting ffplay:", err)
 		return
-	} else {
-		fmt.Println("Started ffplay")
 	}
 	defer ffplayCmd.Process.Kill()
 
 	start := time.Now()
-	_, err = client.Write(GetTCPAuthFrame(connInfo.ConnectionId, connInfo.ClientId))
-	if err != nil {
+	if n, err := client.Write(GetTCPAuthFrame(connInfo.ConnectionId, connInfo.ClientId)); err != nil {
 		fmt.Println("Error sending connection header:", err)
 		return
 	} else {
-		fmt.Println("Connection header sent")
+		fmt.Printf("Sent %d bytes to server\n", n)
 	}
 
-	buf := make([]byte, 1024)
+	buf := make([]byte, 64)
 	for {
-		fmt.Println("Reading from socket...")
+		fmt.Println("Waiting for data from socket...")
 
-		err = client.SetReadDeadline(time.Now().Add(5 * time.Second))
-		if err != nil {
+		if err := client.SetReadDeadline(time.Now().Add(3 * time.Second)); err != nil {
 			fmt.Println("Error setting read deadline:", err)
 			break
 		}
@@ -121,27 +87,47 @@ func TCPStream(connInfo ConnectionDetails) {
 
 		fmt.Printf("Read %d bytes from socket\n", n)
 
-		_, err = ffplayIn.Write(buf[:n])
-		if err != nil {
+		if _, err := inputPipe.Write(buf[:n]); err != nil {
 			fmt.Println("Error writing to ffplay stdin:", err)
 			break
 		}
 
+		// Send a keep-alive ping to the server
 		if time.Since(start) > time.Second {
-			_, err = client.Write(FRAMES_KEEPALIVE)
-			if err != nil {
+			if err := sendPing(client); err != nil {
 				fmt.Println("Error sending keep-alive:", err)
 				break
 			}
+
+			// Reset the timer
 			start = time.Now()
 		}
 	}
 
-	ffplayIn.Close()
+	inputPipe.Close()
 
 	if err := ffplayCmd.Wait(); err != nil {
 		fmt.Println("Error waiting for ffplay:", err)
 	}
 
 	fmt.Println("Stream ended...")
+}
+
+// sendPing sends a keep-alive ping to the server.
+//
+// client: the client connection to send the ping on
+//
+// Example: sendPing(client) = nil
+func sendPing(client *tls.Conn) (err error) {
+	if err := client.SetWriteDeadline(time.Now().Add(3 * time.Second)); err != nil {
+		return fmt.Errorf("error setting write deadline: %w", err)
+	}
+
+	if n, err := client.Write(FRAMES_KEEPALIVE); err != nil {
+		return fmt.Errorf("error sending keep-alive: %w", err)
+	} else {
+		fmt.Printf("Sent %d bytes to server\n", n)
+	}
+
+	return nil
 }
